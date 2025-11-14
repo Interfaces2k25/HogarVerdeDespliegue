d523647543099362a47b87a5686e932ec02317f9
Jessica <jesbromac@alu.edu.gva.es>
2025-11-14 16:54:01 +0100

    app bien

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,3 @@
-// import Contenedor from "./Contenedor";
-// import productos from "./data/productos";
-// import Header from "./Header";
-// import Product from "./Product";
-
-// function App() {
-
-//   return (
-//     <>
-//       <Header />
-
-//       <Contenedor>
-//         <main
-//           className="flex flex-col gap-12 w-full px-6"
-//           aria-labelledby="main-title"
-//         >
-//           <h1 id="main-title" className="sr-only">
-//             Nuestros productos
-//           </h1>
-
-//           {/* Lista sin categorías (ABAJO ESTÁ EL CÓDIGO CUANDO LO MOSTRABA POR CATEGORÍAS)*/}
-//           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-//             {productos.map((p, index) => (
-        
-//                 <Product
-//                   key={index}
-//                   name={p.name}
-//                   price={p.price}
-//                   description={p.description}
-//                   image={p.image}
-//                 />
-            
-//             ))}
-//           </div>
-
-//         </main>
-//       </Contenedor>
-//     </>
-//   );
-// }
-
-// export default App;
-
-
-
 import Contenedor from "./Contenedor"
 import productos from "./data/productos"
 import Header from "./Header"
@@ -90,4 +45,47 @@ function App() {
 }
 
 export default App
+
+// import Contenedor from "./Contenedor";
+// import productos from "./data/productos";
+// import Header from "./Header";
+// import Product from "./Product";
+
+// function App() {
+
+//   return (
+//     <>
+//       <Header />
+
+//       <Contenedor>
+//         <main
+//           className="flex flex-col gap-12 w-full px-6"
+//           aria-labelledby="main-title"
+//         >
+//           <h1 id="main-title" className="sr-only">
+//             Nuestros productos
+//           </h1>
+
+//           {/* Lista sin categorías (ABAJO ESTÁ EL CÓDIGO CUANDO LO MOSTRABA POR CATEGORÍAS)*/}
+//           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+//             {productos.map((p, index) => (
+        
+//                 <Product
+//                   key={index}
+//                   name={p.name}
+//                   price={p.price}
+//                   description={p.description}
+//                   image={p.image}
+//                 />
+            
+//             ))}
+//           </div>
+
+//         </main>
+//       </Contenedor>
+//     </>
+//   );
+// }
+
+// export default App;
 
