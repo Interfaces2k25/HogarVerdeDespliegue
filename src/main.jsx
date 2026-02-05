37088c212f594be06d1e7c6715d15901517edbe9
Jessica <jesbromac@alu.edu.gva.es>
2026-02-05 22:54:25 +0100

    Electron instalado

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,21 @@ import { createRoot } from 'react-dom/client'
 import './assets/styles/index.css'
 import App from './App.jsx'
 import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <>
-    <a
-      href="#main-content"
-      className="sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 bg-blue-700 text-white px-4 py-2 rounded z-50"
-    >
-      Saltar a los productos
-    </a>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
-    </> 
+      <a
+        href="#main-content"
+        className="z-50 px-4 py-2 text-white bg-blue-700 rounded sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4"
+      >
+        Saltar a los productos
+      </a>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </>
   </StrictMode>,
 )
