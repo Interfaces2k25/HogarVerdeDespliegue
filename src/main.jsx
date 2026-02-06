9a171c240010f2520866d10495493786f8a50902
Jessica <jesbromac@alu.edu.gva.es>
2026-02-06 15:30:26 +0100

    fixed ruta productlist

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './assets/styles/index.css'
 import App from './App.jsx'
 import { BrowserRouter } from "react-router-dom";
-import { HashRouter } from "react-router-dom";
 
 
 createRoot(document.getElementById('root')).render(
@@ -15,9 +14,9 @@ createRoot(document.getElementById('root')).render(
       >
         Saltar a los productos
       </a>
-      <HashRouter>
+      <BrowserRouter>
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </>
   </StrictMode>,
 )
